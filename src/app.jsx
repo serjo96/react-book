9691cf72f3b8eb6514fcc4adc09855edbf106f5a
Serjo96 <king-beer@yandex.ru>
2017-07-20 01:41:00 +0500

    change

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import '../styles/index.scss'
-import InputsContainer from './inputs/InputsContainer'
-import BookList from './bookList/booklist'
-
+import AddBook from './containers/AddBook/AddBook'
+import BookList from './containers/BookList/BookList'
+import Book from './containers/Book/Book';
 
 export default class App extends Component {
   render() {
     return (
         <div className="main">
-            <InputsContainer />
+            <AddBook />
             <BookList  />
+            <Book />
         </div>
     )
   }
